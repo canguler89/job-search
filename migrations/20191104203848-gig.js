e95bd24847324c128f06af70ed34b932ac948387
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2019-11-04 15:03:32 -0600

    some changes

--- migrations/20191104203848-gig.js
+++ migrations/20191104203848-gig.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("gigs", {
+    return queryInterface.createTable("gig", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,6 +34,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("gigs");
+    return queryInterface.dropTable("gig");
   }
 };
